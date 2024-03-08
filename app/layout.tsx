9bda4063c56ec9129d846a1cb409f0023a6a2d96
Harith Onigemo <onigemotosin@gmail.com>
2024-03-08 12:37:24 +0400

    :sparkle: Added respond.io to page and updated footer links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Script
+        id="respondio__growth_tool"
+        src="https://cdn.respond.io/widget/widget.js?wId=6ab1a4a3-ebe1-4686-9ff0-b4629ce7acd8"
+      />
+      {/* <!-- https://respond.io --> */}
     </html>
   );
 }
